f8defe44bcc9cf32700200970f081b615c693c7f
Asim Riaz <asim.riaz@gmail.com>
2022-03-15 20:58:56 +0500

    Initial Commit

---
+++ client/src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./style.css"
+
+const App = () => <div>This is App Component</div>
+
+ReactDOM.render(<App />, document.getElementById("root"));
